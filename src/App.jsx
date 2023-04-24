e1ac5eb7be4d67b3df3db514c76ab982c4be7231
cesarlm05 <cesarlm05@gmail.com>
2023-04-24 18:58:21 -0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import Header from "./components/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import { CarritoProvider } from "./context/CarritoContext"
+import Cart from "./components/Cart/Cart";
+
 
 function App() {
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <CarritoProvider>
+          <Header />
+          <Routes>
+            <Route path="/" element={<ItemListContainer />} />
+            <Route path="category/:idCat" element={<ItemListContainer />} />
+            <Route path="item/:id" element={<ItemDetailContainer />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<h1>Sitio en construcción</h1>} />
+          </Routes>
+        </CarritoProvider>
+      </BrowserRouter>
     </>
   );
 }
